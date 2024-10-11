93ee41a5e271890924073a1ab7d020390b8c6a1b
Kwabena Asumadu <codinnovations@gmail.com>
2024-10-11 10:25:02 +0100

    designed the top bar with no functionality

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopBar from "./top-bar";
+
+function HomePage() {
+  return (
+    <>
+      <div>
+		<TopBar/>
+	  </div>
+    </>
+  );
+}
+export default HomePage;
