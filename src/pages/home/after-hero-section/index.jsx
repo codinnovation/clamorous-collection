3738981a4a3b5325ab3944ec577b95f2fb2ad06e
codinnovation <codinnovations@gmail.com>
2024-10-12 19:32:25 +0100

    Revert "modified"

--- src/pages/home/after-hero-section/index.jsx
+++ src/pages/home/after-hero-section/index.jsx
@@ -2,60 +2,51 @@ import React from "react";
 import styles from "../../../styles/home/after-hero-section.module.css";
 import Image from "next/image";
 import HeroImage from "../../../../public/hero-img.png";
-import Fade from "react-reveal/Fade";
 
 function AfterHeroSection() {
   return (
     <>
       <div className={styles.container}>
         <div className={styles.content}>
-          <Fade right>
-            <div className={styles.box}>
-              <div className={styles.image}>
-                <Image src={HeroImage} width={900} height={900} alt="" />
-              </div>
-
-              <div className={styles.description}>
-                <h1>Title Goes Here</h1>
-              </div>
+          <div className={styles.box}>
+            <div className={styles.image}>
+              <Image src={HeroImage} width={900} height={900} alt="" />
             </div>
-          </Fade>
 
-          <Fade bottom>
-            <div className={styles.box}>
-              <div className={styles.image}>
-                <Image src={HeroImage} width={900} height={900} alt="" />
-              </div>
-
-              <div className={styles.description}>
-                <h1>Title Goes Here</h1>
-              </div>
+            <div className={styles.description}>
+              <h1>Title Goes Here</h1>
             </div>
-          </Fade>
+          </div>
 
-          <Fade top>
-            <div className={styles.box}>
-              <div className={styles.image}>
-                <Image src={HeroImage} width={900} height={900} alt="" />
-              </div>
-
-              <div className={styles.description}>
-                <h1>Title Goes Here</h1>
-              </div>
+          <div className={styles.box}>
+            <div className={styles.image}>
+              <Image src={HeroImage} width={900} height={900} alt="" />
             </div>
-          </Fade>
 
-          <Fade left>
-            <div className={styles.box}>
-              <div className={styles.image}>
-                <Image src={HeroImage} width={900} height={900} alt="" />
-              </div>
-
-              <div className={styles.description}>
-                <h1>Title Goes Here</h1>
-              </div>
+            <div className={styles.description}>
+              <h1>Title Goes Here</h1>
             </div>
-          </Fade>
+          </div>
+
+          <div className={styles.box}>
+            <div className={styles.image}>
+              <Image src={HeroImage} width={900} height={900} alt="" />
+            </div>
+
+            <div className={styles.description}>
+              <h1>Title Goes Here</h1>
+            </div>
+          </div>
+
+          <div className={styles.box}>
+            <div className={styles.image}>
+              <Image src={HeroImage} width={900} height={900} alt="" />
+            </div>
+
+            <div className={styles.description}>
+              <h1>Title Goes Here</h1>
+            </div>
+          </div>
         </div>
       </div>
     </>
